397157c4a3b442e1e1ad66a451a4a63be9792cd2
ahmed-debbech <debbech.ahmed@gmail.com>
2020-03-17 21:21:14 +0100

    Add check fields in all pages

--- view/check-fields.js
+++ view/check-fields.js
@@ -1,24 +1,38 @@
+function checkCreate(){
+    if(checkName() == true){
+        location.href = "game-play.html";
+    }
+}
+
 function checkJoin(){
-    checkRoomCode();
-    checkName();
+    if(checkRoomCode() == true&&
+    checkName() == true){
+        location.href = "game-play.html";
+    }
 }
 function checkRoomCode(){
     let r = document.getElementById("roomnum").value;
     if(r == ""){
         alert("You must insert a room code");
+        return false;
     }else{
         if(r.match(/^[A-Z]+$/i) == true){
             alert("Your code must be only numbers");
+            return false;
         }else{
             if(r.length < 4 || r.length > 4){
                 alert("Your code must consist of 4 numbers");
+                return false;
             }
         }
     }
+    return true;
 }
 function checkName(){
     let r = document.getElementById("player-name").value;
     if(r == ""){
         alert("You must write your name!");
+        return false;
     }
+    return true;
 }
