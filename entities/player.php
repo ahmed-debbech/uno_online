f957b51ff93abb3c8c84430a5da964c4d3ac59ff
ahmed-debbech <debbech.ahmed@gmail.com>
2020-04-17 14:01:00 +0100

    Add cards attribute to layer class

--- entities/player.php
+++ entities/player.php
@@ -2,11 +2,13 @@
 class Player {
     private $id;
     private $name;
+    private $numCards;
     private $roomAssigned; //"-1r" if none
     public function __construct($name,$id,$room){
      $this->id = $id;
      $this->name = $name;
      $this->roomAssigned = $room;
+     $this->numCards = 7;
     }
     public function getId(){
         return $this->id;
@@ -19,6 +21,12 @@ class Player {
     }
     public function setAssignedRoom($room){
         $this->roomAssigned = $room;
+    }
+    public function getNumCards(){
+        return $this->numCards;
+    }
+    public function setNumCards($cards){
+        $this->numCards = $cards;
     }
 }
 ?>
--- view/game-play.php
+++ view/game-play.php
@@ -33,11 +33,10 @@ fclose($file);
                                 fclose($file);
                                 $unser = unserialize($content);
                                 $array = $unser->getPlayers();
-                                var_dump($array);
                                 for($i=0; $i<sizeof($array); $i++){
                                     if(unserialize($_SESSION[$_GET["player-id"]])->getName() != $array[$i]->getName()){
                                         echo "<td>";
-                                        echo $array[$i]->getName();
+                                        echo $array[$i]->getName()." - Cards: ".$array[$i]->getNumCards();
                                         echo "</td>";
                                     }
                                 }
