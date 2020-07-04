bbe788bf988ebe5a59de064b16e8528483773fc3
ahmed-debbech <debbech.ahmed@gmail.com>
2020-07-05 00:27:33 +0100

    Fix joinning to a room

--- core/fetch-room.php
+++ core/fetch-room.php
@@ -2,19 +2,54 @@
     session_start();
     include_once("../entities/room.php");
     function checkIntegrity($x){
-        foreach($_SESSION as $player){
-            if(unserialize($player)->getId() == $x){
-                return 0;
-            }
+        $link = mysqli_connect("127.0.0.1", "root", "", "uno_online");
+        $sql = "select * from player where id='".$x."'";
+        $m=0;
+        $res = mysqli_query($link,$sql, $m); 
+        $num= mysqli_num_rows($res);
+        if($num == 0){
+            return 1;
         }
-        return 1;
+        return 0;
     }
-    do{
-        $playerId = rand(1000,9999);
-        $playerId .= "p";
-    }while(checkIntegrity($playerId) == 0);
-    $player = new Player($_GET["player-name"],$playerId,"-1r");
-    $file = fopen("../avail-rooms/".$_GET["roomnum"].".txt", "r");
+    function checkRoomAvailable($x){
+        $link = mysqli_connect("127.0.0.1", "root", "", "uno_online");
+        $sql = "select * from room where roomCode='".$x."'";
+        $m=0;
+        $res = mysqli_query($link,$sql, $m); 
+        $num= mysqli_num_rows($res);
+        if($num == 0){
+            return false;
+        }
+        return true;
+    }
+    function getRemaining($x){
+        $link = mysqli_connect("127.0.0.1", "root", "", "uno_online");
+        $sql = "select * from room where roomCode='".$x."'";
+        $res = mysqli_query($link,$sql); 
+        $list = mysqli_fetch_array($res);
+        return $list["numberOfPlayersRemaining"];
+    }
+    if(checkRoomAvailable($_GET["roomnum"])){
+        $link = mysqli_connect("127.0.0.1", "root", "", "uno_online");
+        $x = getRemaining($_GET["roomnum"])-1;
+        if($x > -1){
+            $sql = "update room set numberOfPlayersRemaining='".$x."' where roomCode='".$_GET["roomnum"]."'";
+            $res = mysqli_query($link,$sql); 
+            do{
+                $playerId = rand(1000,9999);
+                $playerId .= "p";
+            }while(checkIntegrity($playerId) == 0);
+            $player = new Player($_GET["player-name"],$playerId,$_GET["roomnum"]);
+            $player->addPlayerToDB();
+            header("Location: ../view/queue-page.php?player-id=".$playerId."&room-code=".$_GET["roomnum"]);
+        }else{
+            echo "Room is full!";
+        }
+    }else{
+        echo "room not found 404";
+    }
+    /*$file = fopen("../avail-rooms/".$_GET["roomnum"].".txt", "r");
     if($file != FALSE){
         $room = fread($file, filesize("../avail-rooms/".$_GET["roomnum"].".txt"));
         fclose($file);
@@ -41,5 +76,5 @@
         }
     }else{
         echo "room not found 404";
-    }
+    }*/
 ?>
