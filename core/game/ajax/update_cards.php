da40f5c39c7712f7bb1122bbf079b1ea4a2a3b81
ahmed-debbech <debbech.ahmed@gmail.com>
2020-10-22 22:42:32 +0100

    statch changes

--- core/game/ajax/update_cards.php
+++ core/game/ajax/update_cards.php
@@ -13,98 +13,71 @@ function setColors($text){
 function loadCard($content){
     $card = "";
     if($content[0] >= '0' && $content[0] <= '9'){
-        $card = '<html> <head> <style> div.div_war9a_aadiyya{
-            border: 5px solid #E5E7E9;
+        $style1 = "border: 5px solid #E5E7E9;
             border-radius: 20px;
             height: 90px;
             width: 80px;
-            margin: auto;
-         }
-          .war9a_aadiyya{
-              color: white;
+            margin: auto;";
+        $style2 = "color: white;
                 text-align: center;
                 font-size: 50px;
                 text-shadow: 8px 8px 16px #000000;
-             /*position: absolute;  */ 
-             padding-top: 500px;
-            }
-         
-         /*********************** logo ******************/
-         
-         img.logo{
-           width: 25%;
-           padding-left: 10px; 
-         } </style> </head> <body> 
-         <div onclick="setCont('.$content.'); this.parentNode.submit();" name="card" style="background-color: '.setColors($content).';" class="div_war9a_aadiyya">
+                padding-top: 500px;";
+        $card = '<div onclick="setCont('.$content.'); this.parentNode.submit();" name="card" style="'.$style1.' background-color: '.setColors($content).';" class="div_war9a_aadiyya">
             </br>
             <center> 
-                <b class="war9a_aadiyya">'.$content[0].'</b> 
+                <b style="'.$style2.'" class="war9a_aadiyya">'.$content[0].'</b> 
             </center>
-            <img src="assets/res/logo.png" class="logo">
-        </div> </body> </html>';
+            <img style="width: 25%; padding-left: 10px;" src="assets/res/logo.png" class="logo">
+        </div> ';
     }else{
         if(stristr($content, "wc") == true){
-            $card = '<html> <head> <style>
-                div.div_war9et_4_colors{
-                    background-color: black;
-                    border: 5px solid #E5E7E9;
-                    border-radius: 20px;
-                    height: 90px;
-                    width: 80px;
-                }
-                .circle_4_colors{
-                    
-                    width: 10px;
-                    height: 10px;
-                    border-radius: 50%;
-                    background-color: black;
-                    border-top: 20px solid #ed1c24;
-                    border-right: 20px solid green;
-                    border-bottom: 20px solid yellow;
-                    border-left: 20px solid #0F0C93;
-                }
-                </style> </head> <body> 
-                    <div onclick="setCont('.$content.'); this.parentNode.submit();" name="card" class="div_war9et_4_colors">
+            $style1 = "background-color: black;
+            border: 5px solid #E5E7E9;
+            border-radius: 20px;
+            height: 90px;
+            width: 80px;";
+
+            $style2= " width: 10px;
+            height: 10px;
+            border-radius: 50%;
+            background-color: black;
+            border-top: 20px solid #ed1c24;
+            border-right: 20px solid green;
+            border-bottom: 20px solid yellow;
+            border-left: 20px solid #0F0C93;";
+
+            $card = '<div onclick="setCont('.$content.'); this.formcard[0].submit();" style="'.$style1.'" name="card" class="div_war9et_4_colors">
                         <br>
-                        <center>  <div class="circle_4_colors"></div> </center>
+                        <center>  <div style"'.$style2.'" class="circle_4_colors"></div> </center>
                     <img src="assets/res/logo.png" class="logo"> 
-                    </div></body> </html>';
+                    </div>';
 
         }else{
             if(stristr($content, "+2") == true){
-                $card =  '<html> <head> <style>
-                    div.div_war9a_plus2{
-                        border: 5px solid #E5E7E9;
-                        border-radius: 20px;
-                        height: 90px;
-                        width: 80px;
-                        margin: auto;
-                    }
-                    div.icon_plus2{
-                        border: 5px solid;
-                        border-radius: 50px;
-                        height: 50px;
-                        width: 50px;
-                    
-                        animation: mymove 2s infinite;
-                    } 
-                    @keyframes mymove {
+                $style1 = "border: 5px solid #E5E7E9;
+                border-radius: 20px;
+                height: 90px;
+                width: 80px;
+                margin: auto;";
+                $style2 = "border: 5px solid;
+                border-radius: 50px;
+                height: 50px;
+                width: 50px;
+                animation: mymove 2s infinite;
+                @keyframes mymove {
                     50% {box-shadow: 4px 8px 10px white;}
-                    }
-                    
-                    img.icon_plus2{
-                        height: 50px;
-                        width: 50px; 
-                    }
-                    
-                    </style> </head> <body> 
-                    <div onclick="setCont('.$content.'); this.parentNode.submit();" name="card" style="background-color: '.setColors($content).';"  class="div_war9a_plus2">
+                    }";
+
+                $card =  '
+                    <div onclick="setCont('.$content.'); this.formcard[0].submit();" name="card" style="'.$style1.' background-color: '.setColors($content).';"  class="div_war9a_plus2">
                     </br>
-                    <div class="icon_plus2">
-                        <b class="war9a_plus2"><img src="assets/res/two_cards.png" class="icon_plus2"></b> 
+                    <div style="'.$style2.'" class="icon_plus2">
+                        <b class="war9a_plus2"><img src="assets/res/two_cards.png" style="height: 50px;
+                        width: 50px;" class="icon_plus2"></b> 
                     </div>  
                     <img src="assets/res/logo.png" class="logo"> 
-                    </div></body> </html>';
+                    </div>';
 
             }else{
                 if(stristr($content, "+4") == true){
@@ -236,7 +209,7 @@ $r = "<tr>";
 foreach($list as $row){
     $x = loadCard($row["content"]);
     $r .= "<td>";
-    $r = $r ."<form action='core/game/play-card.php' method='get' onsubmit='return is_turn();'>".
+    $r = $r ."<form action='core/game/play-card.php' method='get' name='formcard' onsubmit='return is_turn();'>".
     "<input type='hidden' name='room-code' value='".$_GET["room-code"]."'>".
      "<input type='hidden' name='card-content' value='".$row["content"]."'>".
      "<input type='hidden' name='player-id' value='".$_GET["player-id"]."'>".
